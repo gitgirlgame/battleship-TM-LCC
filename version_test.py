2f0c9735b0db700bcbab2a883ac05c95b948b76d
gitgirlname <victoria.duc088@gmail.com>
2025-03-05 22:17:30 +0100

    Adding input  error solution

--- version_test.py
+++ version_test.py
@@ -7,39 +7,35 @@ ship_robot : int = randint(1,4)
 grille_joueur= [1,2,3,4]
 grille_robot=[1,2,3,4]
 turn = 0
-replay = True
-winner = False
 
 
 def case_joueur():
-    cachette = True
-    case_choisie=int(input("Joueur,choissez sur quelle case vous cachez votre beateau [1|2|3|4]: "))
-    while(cachette==True):
-        if case_choisie in grille_joueur:
-            cachette=False 
-            return case_choisie
-        else :
-            print("Attention, veuillez insérer une case valide !")
-  
-    
-    
+    while True:
+        try:
+            case_choisie = int(input("Joueur, choisissez sur quelle case vous cachez votre bateau [1|2|3|4]: "))
+            if case_choisie in grille_joueur:
+                return case_choisie
+            else:
+                print("Attention, veuillez insérer une case valide !")
+        except ValueError:
+            print("Erreur : Veuillez entrer un nombre entier.")
+
 def joueur_play():
-    entree = True
-    while(entree ==True):
-        choix=int(input(f"Joueur, choisissez sur quelle case votre missile va être lancé {grille_robot} : "))
-        if choix in grille_robot :
-            entree = False
-            if choix==ship_robot :
-                print("Touché, joueur a gagné !")
-                return True
-        
-            else :
-                print("Joueur a raté")
-                grille_robot.remove(choix)
-                return False
-        else:
-            print("Attention, veuillez insérer une case valide !")
-            
+    while True:
+        try:
+            choix = int(input(f"Joueur, choisissez sur quelle case votre missile va être lancé {grille_robot} : "))
+            if choix in grille_robot:
+                if choix == ship_robot:
+                    print("Touché, joueur a gagné !")
+                    return True
+                else:
+                    print("Joueur a raté")
+                    grille_robot.remove(choix)
+                    return False
+            else:
+                print("Attention, veuillez insérer une case valide !")
+        except ValueError:
+            print("Erreur : Veuillez entrer un nombre entier.")
             
 
 
@@ -57,7 +53,7 @@ def robot_play():
 
 print("Jeu 2 par 2 de la bataille naval \n ------------------------------------------")
 ship_joueur = case_joueur()
-while(replay == True):
+while True:
     if turn%2 == 0 :
         winner=joueur_play()
 
@@ -65,7 +61,7 @@ while(replay == True):
         winner=robot_play()
 
     if winner == True:
-        replay = False
+        False
     else:
         turn += 1
     
